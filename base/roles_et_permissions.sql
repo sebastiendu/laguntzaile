f79be5bfd8b2efb5d7c00896f044f98adb0ce07d
Sébastien Ducoulombier <seb@ldd.fr>
2015-05-04 13:51:12 +0200

    Ajouté le droit d'utiliser les sequences

--- base/roles_et_permissions.sql
+++ base/roles_et_permissions.sql
@@ -1,2 +1,3 @@
 create role administration;
 grant all on all tables in schema public to administration; -- FIXME : restrict a bit more
+grant all on all sequences in schema public to administration;
