a2473c79fb063f770d24aeaa78550567dc29708d
Sébastien Ducoulombier <seb@ldd.fr>
2015-05-08 18:21:09 +0200

    poste_et_tour : nombre d'affectations corrigé

--- base/vues.sql
+++ base/vues.sql
@@ -581,19 +581,19 @@ group by heure;
 -- Gestion des postes, tours, et affectations, pour l'affichage sur le plan
 
 create or replace view poste_et_tour as -- pour affichage du plan
-select 
+select
 id_poste,
-poste.id_evenement, 
-poste.nom, 
-poste.description, 
+poste.id_evenement,
+poste.nom,
+poste.description,
 poste.posx,
-poste.posy, 
+poste.posy,
 tour.debut,
 id_tour,
 tour.fin,
-tour.min, 
+tour.min,
 tour.max,
-count(*)  as nombre_affectations,
+count(affectation.id)  as nombre_affectations,
 sum(case when affectation.statut = 'possible' then 1 else 0 end) as nombre_affectations_possibles,
 sum(case when affectation.statut = 'proposee' then 1 else 0 end) as nombre_affectations_proposees,
 sum(case when affectation.statut in ('validee', 'acceptee') then 1 else 0 end) as nombre_affectations_validees_ou_acceptees
